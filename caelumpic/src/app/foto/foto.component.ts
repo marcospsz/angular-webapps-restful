bc95ad9e6e4d296f34601541b2a72c3d3cf048f2
Marcos <marcospsz@gmail.com>
2018-11-05 22:59:09 -0200

    primeira aula 05/11

---
+++ caelumpic/src/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'foto',
+  template: '<img src= "/assets/img/leao.jpeg" alt="Leao">'
+
+})
+export class FotoComponent {
+
+}
